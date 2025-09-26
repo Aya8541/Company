3c0924c60abe4b3aa4e48efa5cf580a830abdb0e
Aya <Aya@DESKTOP-OPH4DDB>
2025-09-26 14:31:04 +0300

    Create Create(Action-View)

--- Company.G02.PL/Controllers/DepartmentController.cs
+++ Company.G02.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.G02.BLL.Interfaces;
 using Company.G02.BLL.Repositories;
+using Company.G02.DAL.Models;
+using Company.G02.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G02.PL.Controllers
@@ -21,6 +23,32 @@ namespace Company.G02.PL.Controllers
             var departments = _departmentRepository.GetAll();
 
             return View(departments);
+        }
+        [HttpGet] //Get: /Department/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid) //Server side validation
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt,
+                };
+
+              var count =  _departmentRepository.Add(department);
+                if (count > 0 )
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
         }
     }
 }
